a7cd2f8cbc20faeddcf7a14b124dff55e43390d4
RadidaR <radidar19@yahoo.com>
2021-11-09 20:07:39 +0000

    Audio

--- Scream Jam 2021/Assets/0 PROJECT/Scripts/GameplayAudioScript.cs
+++ Scream Jam 2021/Assets/0 PROJECT/Scripts/GameplayAudioScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DarkTonic.MasterAudio;
+using Sirenix.OdinInspector;
 
 
 namespace ScreamJam
@@ -11,10 +12,29 @@ namespace ScreamJam
         [SerializeField] GameData data;
         [SoundGroupAttribute][SerializeField] string footstepsGroup;
 
+
+        [SoundGroupAttribute] [SerializeField] string gameplayMusicGroup;
+        float _gameplayMusicDefaultVolume;
+
+        [SoundGroupAttribute] [SerializeField] string chaseMusicGroup;
+        float _chaseMusicDefaultVolume;
+
+
+        [SerializeField] GameEvent _event;
+
         bool _isMoving;
         bool _wasMoving;
 
         bool _wasUsingStairs;
+
+        bool _wasSeen;
+
+        private void Start()
+        {
+            PlaySound(gameplayMusicGroup);
+            _gameplayMusicDefaultVolume = MasterAudio.GetGroupVolume(gameplayMusicGroup);
+            _chaseMusicDefaultVolume = MasterAudio.GetGroupVolume(chaseMusicGroup);
+        }
 
         private void Update()
         {
@@ -22,11 +42,29 @@ namespace ScreamJam
 
             if ((!_wasMoving && _isMoving) || data.usingStair /*&& !_wasUsingStairs)*/)
             {
-                MasterAudio.PlaySound(footstepsGroup, 1);
+                PlaySound(footstepsGroup);
             }
             else if ((_wasMoving && !_isMoving) || (!data.usingStair && _wasUsingStairs))
             {
-                MasterAudio.StopAllOfSound(footstepsGroup);
+                StopSound(footstepsGroup);
+            }
+
+            if (data.inSight && !_wasSeen)
+            {
+                FadeSound(gameplayMusicGroup, 0.05f, 0.35f);
+                PlaySound(chaseMusicGroup);
+                FadeSound(chaseMusicGroup, _chaseMusicDefaultVolume, 0.35f);
+            }
+            else if (!data.inSight && _wasSeen)
+            {
+                FadeSound(chaseMusicGroup, 0f, 0.35f);
+                FadeSound(gameplayMusicGroup, _gameplayMusicDefaultVolume, 0.35f);
+            }
+
+            if (MasterAudio.GetGroupVolume(chaseMusicGroup) == 0)
+            {
+                StopSound(chaseMusicGroup);
+                MasterAudio.SetGroupVolume(chaseMusicGroup, _chaseMusicDefaultVolume);
             }
         }
 
@@ -34,6 +72,7 @@ namespace ScreamJam
         {
             _wasMoving = _isMoving;
             _wasUsingStairs = data.usingStair;
+            _wasSeen = data.inSight;
         }
 
         public void PlaySound(string soundGroup)
@@ -41,9 +80,30 @@ namespace ScreamJam
             MasterAudio.PlaySoundAndForget(soundGroup);
         }
 
+        public void FadeSound(string soundGroup, float toVolume, float overTime)
+        {
+            MasterAudio.FadeSoundGroupToVolume(soundGroup, toVolume, overTime);
+        }
+
+        public void FadeGameMusic(float time)
+        {
+            MasterAudio.FadeSoundGroupToVolume(gameplayMusicGroup, 0.05f, time);
+        }
+
+        public void RestoreGameMusic(float time)
+        {
+            MasterAudio.FadeSoundGroupToVolume(gameplayMusicGroup, _gameplayMusicDefaultVolume, time);
+        }
+
         public void StopSound(string soundGroup)
         {
             MasterAudio.StopAllOfSound(soundGroup);
+        }
+
+        [Button("Raise Event")]
+        void Raise()
+        {
+            _event.Raise();
         }
 
     }
